3aa4d8bf771c45cb140a646d9f257e7efffe9700
TT-HR <yhr199792@gmail.com>
2019-01-16 17:59:28 +0800

    service

---
+++ src/main/java/cn/tt/controller/UserService.java
@@ -0,0 +1,22 @@
+package cn.tt.controller;
+
+import cn.tt.bean.User;
+import cn.tt.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService implements UserMapper{
+    @Autowired
+    private UserMapper userMapper;
+
+    public List<User> login(String username, String pwd) {
+        return userMapper.login(username,pwd);
+    }
+
+    public void reg(User user) {
+        userMapper.reg(user);
+    }
+}
---
+++ src/main/java/cn/tt/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package cn.tt.mapper;
+
+import cn.tt.bean.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+public interface UserMapper {
+    @Select("select * from user where username=#{username} and pwd=#{pwd}")
+    List<User> login(String username,String pwd);
+    @Insert("insert into user(username,pwd) values(#{username},#{pwd})")
+    void reg(User user);
+}
